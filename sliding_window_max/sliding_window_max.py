b15694b2f424eac4ef2cbd96266a8322b1aed32d
Virginia Davenport <virginialdport@gmail.com>
2020-08-19 15:57:30 -0600

    mvp on first pass solutions

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,28 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+#window is going to be a range == value of k
+#with each move the window makes the highest value will be recorded
+#the highest values will be returned to a new list
 def sliding_window_max(nums, k):
-    # Your code here
+    #create an array to store the max values
+    #iterate through the range of values determined by k
+    #find the highest value in the range 
+    #shift the range
+    max_values = []
 
-    pass
+    start = 0
+    end = k 
+
+    while end <= len(nums):
+        max_values.append(max(nums[start:end]))
+        start += 1
+        end += 1
+    
+    return max_values
+
+
+
 
 
 if __name__ == '__main__':
