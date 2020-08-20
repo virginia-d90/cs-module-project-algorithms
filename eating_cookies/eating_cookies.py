15c732802dda075802fa753e45b78ec324f6d8af
Virginia Davenport <virginialdport@gmail.com>
2020-08-20 15:30:47 -0600

    finally got cached eating_cookies to work

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -21,19 +21,26 @@ Returns: an integer
 #     else:
 #         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-def eating_cookies(n, cache={}):
+def eating_cookies(n, cache=None):
+        # base case
+     if n == 0:
+         return 1
+     elif n < 0:
+         return 0
+     # check if the work has already been done
+     # by looking in the cache
+     elif cache is not None and cache[n] > 0:
+         # return the previously computed answer and don't recurse
+         return cache[n]
+     else:
+         # might need to create our cache for the first time
+         if cache is None:
+             # initialize an empty list for a cache based off of testing
+             cache = [0 for i in range(n + 1)]
+             # store the value of the recursive call expression in out cache
+         cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
 
-    if n < 0:
-        return 0
-    if n == 0:
-        return 1
-    elif n in cache:
-        return cache[n]
-    else:
-        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
-    return cache[n]
-
-    
+     return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
